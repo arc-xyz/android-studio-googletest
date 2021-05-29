5477132fded935b33a6e473a5409393a8cc265b9
yong <yong@arctuition.com>
2021-05-29 09:09:08 +0800

    add build script

---
+++ app/src/main/cpp/x86.sh
@@ -0,0 +1,34 @@
+BUILD_DIR=build_android
+
+# default to "Debug" config
+CONFIG=Debug
+if [ -n "$1" ]; then
+    CONFIG=$1
+fi
+
+MINSDKVERSION=16
+
+# Change the NDK_ROOT to the path where NDK is installed. 
+NDK_ROOT=/Users/zhaoyong/Downloads/android-ndk-r22b
+
+rm -rf $BUILD_DIR
+mkdir $BUILD_DIR
+
+ABI=x86
+
+cmake -S . -B $BUILD_DIR -DANDROID_NDK=$NDK_ROOT \
+    -DCMAKE_TOOLCHAIN_FILE=$NDK_ROOT/build/cmake/android.toolchain.cmake \
+    -DANDROID_ABI=$ABI \
+    -DANDROID_NATIVE_API_LEVEL=$MINSDKVERSION \
+    -DCMAKE_BUILD_TYPE=$CONFIG \
+    -Wno-error=deprecated-declarations
+
+
+pushd $BUILD_DIR
+cmake --build . --config $CONFIG
+#cmake --install . --prefix ../../android/app/src/main/JniLibs/arm64-v8a
+#cp ../bin/*.so ../../android/app/src/main/JniLibs/arm64-v8a/
+popd
+
+popd
+
